b4160f5ee3ce0be60895ed4dd0a44c9aae435f34
HelenBlyzniuk <115355592+HelenBlyzniuk@users.noreply.github.com>
2023-06-26 18:51:49 +0300

    fixed

--- src/components/redux/thunks.js
+++ src/components/redux/thunks.js
@@ -110,33 +110,5 @@ export const fetchContactsThunk= createAsyncThunk("contactss/fetchAll", async ()
     }
       })
       
-//       axios.defaults.baseURL = "https://648c54b28620b8bae7ecb565.mockapi.io/";
 
-// export const fetchContacts= createAsyncThunk("contactss/fetchAll", async () => {
-//     const response = await axios.get("/contacts");
-//     return response.data;
-//   });
-
-
-//   export const addContact = createAsyncThunk("contacts/addContact",async ({createdAt,name,phone,id},thunkAPI)=>{
-//     try {
-//        const response=await axios.post("/contacts",{createdAt,name,phone,id});
-     
-//        return response.data; 
-//     } catch (error) {
-//         return thunkAPI.rejectWithValue(error.message)
-//     }
-//   })
-
-//   export const deleteContact =createAsyncThunk("contacts/deleteContact",async(id,thunkAPI)=>{
-//     try {
-//         const response=await axios.delete(`/contacts/${id}`)
-//         return response.data; 
-         
-//     } catch (error) {
-//         return thunkAPI.rejectWithValue(error.message)
-//     }
-      
-//       } )
-  
 
