427f3945c5b811c6e4e3df8b83a2064ff7a17f6e
HelenBlyzniuk <115355592+HelenBlyzniuk@users.noreply.github.com>
2023-06-26 17:42:25 +0300

    исправила url

--- src/app/app.js
+++ src/app/app.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const instance = axios.create({
-  baseURL: 'https:/connections-api.herokuapp.com/',
+  baseURL: 'https://connections-api.herokuapp.com',
 });
 
 export const setToken = token => {
